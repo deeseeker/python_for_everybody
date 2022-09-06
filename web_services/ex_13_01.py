b732b4ea30b38c9e000c8cb7c79027bcb4c85216
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2022-09-06 10:20:27 +0100

    reads data from the server

---
+++ web_services/ex_13_01.py
@@ -0,0 +1,22 @@
+#import necessary libraries
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    #prompts user for input
+    url = input('Enter location:')
+    if len(url) < 1: break
+
+    #sends a call to the server
+    uh = urllib.request.urlopen(url, context=ctx)
+    #reads the response in form of bits
+    data = uh.read()
+    print('Retrieved', len(data), 'characters')
+    #decodes
+    print(data.decode())
