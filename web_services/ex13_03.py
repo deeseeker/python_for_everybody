60b751a5daa8b19c55298071c53893f4dd9d01bb
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2022-09-14 11:53:46 +0100

    geojson project - the first part

---
+++ web_services/ex13_03.py
@@ -0,0 +1,28 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import SSL
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+
+while True:
+    location = input('Enter Location-')
+    if len(address) < 1: break
+    params = dict()
+    params['location'] = location
+    if api_key is not False: params['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(params)
+
+    print('Retrieving', url)
