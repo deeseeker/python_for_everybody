7a9f5f2828f3021de026287db068cbf81f429754
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2022-09-14 12:21:16 +0100

    added the sll certificate errors

--- web_services/ex13_03.py
+++ web_services/ex13_03.py
@@ -3,7 +3,8 @@ import json
 import ssl
 
 api_key = False
-# If you have a Google Places API key, enter it here
+# If you have a Google Places API key, enter
+it here
 api_key = 'AIzaSy___IDByT70'
 # https://developers.google.com/maps/documentation/geocoding/intro
 
@@ -14,8 +15,11 @@ else :
     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 # Ignore SSL certificate errors
+# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
 
 while True:
     location = input('Enter Location-')
